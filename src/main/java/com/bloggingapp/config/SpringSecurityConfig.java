3b6a98a70b73e0d3220c3c0e83abfc18eeeb78b5
Razaa Faiyazi <razaafaiyazi02@gmail.com>
2024-09-26 09:27:27 +0530

    Added Spring Security and did in memory authentication also did role based authorization on Post

---
+++ src/main/java/com/bloggingapp/config/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.bloggingapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    //enabling only Basic authentication
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf)->csrf.disable())
+                .authorizeHttpRequests((authorize)->{
+                    //role based authorization
+                    authorize.requestMatchers(HttpMethod.POST,"/api/posts/**").hasAnyRole("ADMIN","USER");
+                    authorize.requestMatchers(HttpMethod.PUT,"/api/posts/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE,"/api/posts/**").hasRole("ADMIN");
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    // In-memory authentication
+    @Bean
+    UserDetailsService userDetailsService(){
+        UserDetails razaa = User.builder()
+                .username("razaa")
+                .password(this.passwordEncoder().encode("razaa"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(this.passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(razaa,admin);
+    }
+}
--- src/main/java/com/bloggingapp/controller/CommentController.java
+++ src/main/java/com/bloggingapp/controller/CommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/comments")
 public class CommentController {
     private CommentService commentService;
 
