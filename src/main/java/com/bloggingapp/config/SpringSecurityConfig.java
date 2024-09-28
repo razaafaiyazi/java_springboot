9eed884bca64b295373e62979d1ba7f4fcb52e89
Razaa Faiyazi <razaafaiyazi02@gmail.com>
2024-09-28 10:36:51 +0530

    ADDED METHOD LEVEL SECURITY IN POST CONTROLLER

--- src/main/java/com/bloggingapp/config/SpringSecurityConfig.java
+++ src/main/java/com/bloggingapp/config/SpringSecurityConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SpringSecurityConfig {
 
     @Bean
@@ -26,10 +27,12 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf)->csrf.disable())
                 .authorizeHttpRequests((authorize)->{
+                    /*COMMENTED BELOW CODES TO ENABLE METHOD LEVEL AUTHORIZATION*/
                     //role based authorization
-                    authorize.requestMatchers(HttpMethod.POST,"/api/posts/**").hasAnyRole("ADMIN","USER");
-                    authorize.requestMatchers(HttpMethod.PUT,"/api/posts/**").hasRole("ADMIN");
-                    authorize.requestMatchers(HttpMethod.DELETE,"/api/posts/**").hasRole("ADMIN");
+         //           authorize.requestMatchers(HttpMethod.POST,"/api/posts/**").hasAnyRole("ADMIN","USER");
+         //           authorize.requestMatchers(HttpMethod.PUT,"/api/posts/**").hasRole("ADMIN");
+         //           authorize.requestMatchers(HttpMethod.DELETE,"/api/posts/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET,"/api/posts/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
         return http.build();
--- src/main/java/com/bloggingapp/controller/PostController.java
+++ src/main/java/com/bloggingapp/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class PostController {
     @Autowired
     private PostService postService;
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<PostDto> createPost(@RequestBody @Valid PostDto postDto){
         PostDto postDto1 = postService.savePost(postDto);
         return new ResponseEntity<>(postDto1, HttpStatus.CREATED);
@@ -38,15 +40,17 @@ public class PostController {
         return new ResponseEntity<>(postDto,HttpStatus.CREATED);
     }
     @PutMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto){
         PostDto updatedPost = postService.updatePost(postDto);
         return new ResponseEntity<>(updatedPost,HttpStatus.CREATED);
     }
     //delete post by id
     @DeleteMapping("/{id}")
-    public String deletePostById(@PathVariable("id") int id){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> deletePostById(@PathVariable("id") int id){
         String deleteMsg = postService.deletePost(id);
-        return String.valueOf(new ResponseEntity<>(deleteMsg,HttpStatus.OK));
+        return new ResponseEntity<>(deleteMsg,HttpStatus.OK);
     }
 
 }
